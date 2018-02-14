522dd907c06b231b2c663ee9abe2d2c003ea10e1
joebos <frank_lqx@yahoo.com>
2018-02-13 20:11:10 -0500

    ...

--- feedback_form/forms.py
+++ feedback_form/forms.py
@@ -3,9 +3,13 @@ from django import forms
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
-from django_libs.utils_email import send_email
+#from django_libs.utils_email import send_email
 
 from .models import Feedback
+from django.template.loader import render_to_string
+from django.template import TemplateDoesNotExist
+from django.core.mail import EmailMultiAlternatives, EmailMessage
+from django.core.mail import get_connection
 
 
 class FeedbackForm(forms.ModelForm):
@@ -35,17 +39,19 @@ class FeedbackForm(forms.ModelForm):
             self.instance.content_object = self.content_object
 
             obj = super(FeedbackForm, self).save()
-            send_email(
-                '',
-                {
-                    'url': reverse('admin:feedback_form_feedback_change',
-                                   args=(obj.id, )),
-                    'feedback': obj,
-                },
+            context = {
+                'url': reverse('admin:feedback_form_feedback_change',
+                               args=(obj.id, )),
+                'feedback': obj,
+            }
+            self.send_mail(
+
                 'feedback_form/email/subject.html',
                 'feedback_form/email/body.html',
-                from_email=self.instance.email,
-                recipients=[manager[1] for manager in settings.MANAGERS],
+                self.instance.email,
+                [support[1] for support in settings.SUPPORT],
+                [],
+                context
             )
             return obj
 
@@ -56,3 +62,47 @@ class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ('email', 'message')
+
+    def render_mail_with_template(self, subject_template, body_template, from_email, tos, bccs, context):
+        """
+        Renders an e-mail to `email`.  `template_prefix` identifies the
+        e-mail that is to be sent, e.g. "account/email/email_confirmation"
+        """
+        subject = render_to_string(subject_template, context)
+        # remove superfluous line breaks
+        subject = " ".join(subject.splitlines()).strip()
+        #subject = self.format_email_subject(subject)
+
+        bodies = {}
+        for ext in ['html', 'txt']:
+            try:
+                template_name = body_template
+                bodies[ext] = render_to_string(template_name, context).strip()
+            except TemplateDoesNotExist:
+                if ext == 'txt' and not bodies:
+                    # We need at least one body
+                    raise
+        if 'txt' in bodies:
+            msg = EmailMultiAlternatives(subject,
+                                         bodies['txt'],
+                                         from_email=from_email,
+                                         to=tos,
+                                         bcc=bccs)
+            if 'html' in bodies:
+                msg.attach_alternative(bodies['html'], 'text/html')
+        else:
+            msg = EmailMessage( subject,
+                                bodies['html'],
+                                from_email=from_email,
+                                to=tos,
+                                bcc=bccs)
+            msg.content_subtype = 'html'  # Main content is now text/html
+        return msg
+
+    def send_mail(self, subject_template, body_template, from_email, tos, bccs, context):
+        msg = self.render_mail_with_template(subject_template, body_template, from_email, tos, bccs, context)
+
+        msg.connection = get_connection('django.core.mail.backends.smtp.EmailBackend')
+        msg.send()
+        msg.connection.close()
+
